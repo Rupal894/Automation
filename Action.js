5aa2662318d7805280974bcaad79c08cbca87160
Archana Durgam <archana.durgam@ge.com>
2020-06-05 13:41:37 +0530

    Updated Action.js

--- Action.js
+++ Action.js
@@ -1,1 +1,21 @@
+///<reference types="Cypress" />
+//<reference *types*="cypress-shadow-dom" />
 
+before(() => {
+    cy.visit('/')
+    cy.get('#username').type('BHGE-repairengineer-1')
+    cy.get('#password').type('H*^m*n3T5d')
+    cy.get('.btn').click().as('Login')
+})
+describe('Login to the application', () => {
+
+    it('Action', () => {
+
+        cy.wait(20000)
+        cy.shadowGet('#trigger')
+            .shadowFind('.dropdown-trigger')
+            .shadowClick()
+    })
+
+
+})
